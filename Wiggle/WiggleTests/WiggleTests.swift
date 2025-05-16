aa0a2b3f0fc10117ca436f48586c69d9baa035ea
jikbb2 <jikbb2@gmail.com>
2025-05-16 20:46:48 +0900

    [Config] Add project setting for Immersive Environment App.

---
+++ Wiggle/WiggleTests/WiggleTests.swift
@@ -0,0 +1,17 @@
+//
+//  WiggleTests.swift
+//  WiggleTests
+//
+//  Created by byeoungjik on 5/16/25.
+//
+
+import Testing
+@testable import Wiggle
+
+struct WiggleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
